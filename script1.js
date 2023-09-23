d4ec180055e96743ec8e7ffd275d8c8a5dbe84ac
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-09-23 12:54:37 +0530

    data from local storage  working

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 const logout = document.getElementById("logout");
 
 logout.addEventListener('click', function(){
-    sessionStorage.clear();
+    localStorage.clear();
     alert("You are successfully logout");
     window.location.href = "index.html";
 });
